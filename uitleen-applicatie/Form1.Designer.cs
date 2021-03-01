c92c900e3d481aa401ed3d760264ae173e69d27b
supr3mion <230690@edu.rocfriesepoort.nl>
2021-03-01 10:03:19 +0100

    form1 begin

--- uitleen-applicatie/Form1.Designer.cs
+++ uitleen-applicatie/Form1.Designer.cs
@@ -29,13 +29,89 @@ namespace uitleen_applicatie
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Naam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SerieNummer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Beschrijving = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DatumRetour = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Naam,
+            this.SerieNummer,
+            this.Beschrijving,
+            this.Status,
+            this.DatumRetour});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 371);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Naam
+            // 
+            this.Naam.Text = "Naam";
+            this.Naam.Width = 122;
+            // 
+            // SerieNummer
+            // 
+            this.SerieNummer.Text = "Serie nummer";
+            this.SerieNummer.Width = 125;
+            // 
+            // Beschrijving
+            // 
+            this.Beschrijving.Text = "Beschrijving";
+            this.Beschrijving.Width = 299;
+            // 
+            // Status
+            // 
+            this.Status.Text = "Status";
+            this.Status.Width = 107;
+            // 
+            // DatumRetour
+            // 
+            this.DatumRetour.Text = "Datum van retour";
+            this.DatumRetour.Width = 119;
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(608, 401);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(180, 37);
+            this.add.TabIndex = 1;
+            this.add.Text = "+ Toevoegen";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Naam;
+        private System.Windows.Forms.ColumnHeader SerieNummer;
+        private System.Windows.Forms.ColumnHeader Beschrijving;
+        private System.Windows.Forms.ColumnHeader Status;
+        private System.Windows.Forms.ColumnHeader DatumRetour;
+        private System.Windows.Forms.Button add;
     }
 }
 
---
+++ uitleen-applicatie/mysql.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uitleen_applicatie
+{
+    public partial class mysql : Component
+    {
+        public mysql()
+        {
+            InitializeComponent();
+        }
+
+        public mysql(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
