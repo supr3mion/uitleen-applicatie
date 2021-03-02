b27921ae97a1ac0e7afd44c9edcd5471ad42cbac
supr3mion <230690@edu.rocfriesepoort.nl>
2021-03-02 15:16:56 +0100

    size gekorrigeerd

--- uitleen-applicatie/Form2.Designer.cs
+++ uitleen-applicatie/Form2.Designer.cs
@@ -38,49 +38,54 @@ namespace uitleen_applicatie
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(668, 316);
+            this.label1.Location = new System.Drawing.Point(334, 164);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "apparatuur :";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(627, 360);
+            this.textBox1.Location = new System.Drawing.Point(314, 187);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(214, 32);
+            this.textBox1.Size = new System.Drawing.Size(109, 19);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "    naam apparatuur:\r\n\r\n\r\n";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(627, 398);
+            this.textBox2.Location = new System.Drawing.Point(314, 207);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(214, 32);
+            this.textBox2.Size = new System.Drawing.Size(109, 19);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "    serie nummer:\r\n\r\n\r\n";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(627, 436);
+            this.textBox3.Location = new System.Drawing.Point(314, 227);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(214, 36);
+            this.textBox3.Size = new System.Drawing.Size(109, 21);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "    beschrijving:";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(1602, 893);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
