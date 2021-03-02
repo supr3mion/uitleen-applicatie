5cb546999af357e68e1e177ac6a985dd5f849ce3
supr3mion <230690@edu.rocfriesepoort.nl>
2021-03-02 15:14:15 +0100

    form Overzicht toegevoegd

---
+++ uitleen-applicatie/Overzicht.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uitleen_applicatie
+{
+    public partial class Overzicht : Form
+    {
+        public Overzicht()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
