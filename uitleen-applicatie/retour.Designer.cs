c84891cbfd4bc854c6e1be17324680fdfa7284a7
Marthijs1 <75075864+Marthijs1@users.noreply.github.com>
2021-03-04 12:30:51 +0100

    Form Retourneren

--- uitleen-applicatie/retour.Designer.cs
+++ uitleen-applicatie/retour.Designer.cs
@@ -29,7 +29,49 @@ namespace uitleen_applicatie
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbCommentaar = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // tbCommentaar
+            // 
+            this.tbCommentaar.Location = new System.Drawing.Point(257, 93);
+            this.tbCommentaar.Multiline = true;
+            this.tbCommentaar.Name = "tbCommentaar";
+            this.tbCommentaar.Size = new System.Drawing.Size(246, 157);
+            this.tbCommentaar.TabIndex = 7;
+            this.tbCommentaar.Text = "                             Commetaar:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(257, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Opslaan";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(415, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Sluiten";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Retourneren";
             // 
             // retour
             // 
@@ -37,12 +79,22 @@ namespace uitleen_applicatie
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbCommentaar);
             this.Name = "retour";
             this.Text = "retour";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbCommentaar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
